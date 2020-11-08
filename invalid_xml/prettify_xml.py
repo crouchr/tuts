9d91074daa443dd702cc8d5f3daa9939e7c62a3d
crouchr <richard.crouch@vonage.com>
2020-11-08 22:34:04 +0000

    TUTS bug found

--- invalid_xml/prettify_xml.py
+++ invalid_xml/prettify_xml.py
@@ -2,7 +2,6 @@ import xmlschema
 from lxml import etree
 
 
-
 def validate_xml_1(xml_file, xsd_file):
     """
 
@@ -18,9 +17,8 @@ def validate_xml_1(xml_file, xsd_file):
 
     return True, None
 
-
-# example of output : Unescaped '<' not allowed in attributes values, line 114, column 60 (bad-xml-from-tuts.xml, line 114)
-def validate_xml_2(xml_file, xsd_file):
+# This is more informative
+def validate_xml(xml_file, xsd_file):
     """
 
     :param xml_file:
@@ -41,16 +39,14 @@ def validate_xml_2(xml_file, xsd_file):
 
 def prettify_unit_tests_results(in_file, out_file):
     lines_processed = 0
+    doc_string = '"' + '"' + '"'
     f_out = open(out_file, 'w')
+
     with open(in_file) as f_in:
-
-    #lines = f_in.readlines()
-    #print('len of lines is ' + len(lines).__str__())
-
         for line in f_in:
             pretty_line = line
-            #if '"""' in pretty_line:
-            #    pretty_line = pretty_line.replace('"""', '')
+            if doc_string in pretty_line:
+                pretty_line = pretty_line.replace(doc_string, '')
             if '@' in pretty_line:
                 pretty_line = pretty_line.replace('@', '"')
             if '<testcase' in pretty_line:
@@ -59,8 +55,6 @@ def prettify_unit_tests_results(in_file, out_file):
                 pretty_line = pretty_line.replace('</testcase', "\n" + '</testcase')
             if '<skipped' in pretty_line:
                 pretty_line = pretty_line.replace('<skipped', "\n" + '<skipped')
-            if '</skipped' in pretty_line:
-                pretty_line = pretty_line.replace('</skipped', "\n" + '</skipped')
             if '<testsuit' in pretty_line:
                 pretty_line = pretty_line.replace('<testsuit', "\n" + '<testsuit')
             if '</testsuite' in pretty_line:
@@ -73,39 +67,34 @@ def prettify_unit_tests_results(in_file, out_file):
                 pretty_line = pretty_line.replace('&gt;', '')
             if '&#10;' in pretty_line:
                 pretty_line = pretty_line.replace('&#10;', '')
+            #if pretty_line[0] != 'E' and pretty_line[0] != '<':
+            #    continue
+            if pretty_line[0] == 'E' and 'assert False' in pretty_line:
+                continue
+            if pretty_line[0] == 'E' and 'AssertionError:' in pretty_line:
+                continue
             lines_processed += 1
             f_out.write(pretty_line)
 
-            print('*** ' + pretty_line.rstrip())
+            #print(pretty_line.rstrip())
 
-    #f_in.close()
     f_out.close()
-    print("\n" + 'finished')
-    print(lines_processed)
 
 
 if __name__ == '__main__':
     xsd_file = 'schema/junit10.xsd'
-    #xsd_file = 'schema/jenkins.xsd'
 
     xml_file = 'artifacts/bad-xml-from-tuts.xml'
-    #xml_file = 'artifacts/good-xml-from-tuts.xml'
-    #xml_file = 'artifacts/test_results.xml'
+    pretty_file = 'artifacts/prettified.xml'
 
-    prettify_unit_tests_results(xml_file, 'artifacts/prettified.xml')
+    prettify_unit_tests_results(xml_file, pretty_file)
 
-    result_1, err_msg_1 = validate_xml_1('artifacts/prettified.xml', xsd_file)
-    result_2, err_msg_2 = validate_xml_2('artifacts/prettified.xml', xsd_file)
+    result, err_msg = validate_xml('artifacts/prettified.xml', xsd_file)
 
     print('Schema : ' + xsd_file)
-    print('Results file : ' + xml_file)
+    print('Results file : ' + pretty_file)
 
-    if result_1:
-        print("Valid XML")
+    if result:
+        print("SUCCESS : Valid XML")
     else:
-        print("Invalid XML : " + err_msg_1.__str__())
-
-    if result_2:
-        print("Valid XML")
-    else:
-        print("Invalid XML : " + err_msg_2.__str__())
+        print("Invalid XML : " + err_msg.__str__())
